109c2f6e57c8980eb5f24acc0d6f8e699bbfba3a
gian910147 <gian910147@gmail.com>
2022-03-25 00:07:59 +0800

    renamed variable

--- app.js
+++ app.js
@@ -4,38 +4,39 @@ const ul = document.querySelector(".siteList")
 
 
 document.querySelector("#searchForm").addEventListener("submit" , (e) =>{
-  const textInput = document.querySelector("#searchKeyword")
   e.preventDefault()
+  const textInput = document.querySelector("#searchKeyword")
+
+
   if(textInput.value.trim() !== ""){
     getData(textInput.value)
   }else
     return alert("請輸入文字")
+
   ul.textContent = ""    
+  textInput.focus()
 })
 
 async function getData(value) {
   const rawData = await fetch(API)
   const bikes = await rawData.json()
+  const ul = document.querySelector(".siteList")
 
+  const datas = bikes.filter((element) => element.ar.includes(value))
 
-  const data = bikes.filter((ele)=> {
-    if((ele.ar).includes(value)){
-      return ele
-    }
-  })
-
-  data.forEach((x) => {
-    const a =  createdData(x)
-    ul.insertAdjacentHTML("afterbegin" ,a)
+  datas.forEach((data) => {
+    const selectData =  createdLi(data)
+    ul.insertAdjacentHTML("afterbegin", selectData)
   });  
 }
 
 
-function createdData(x)  {  
-  const bike =`<li class="list-group-item fs-5">
+function createdLi(x)  {  
+  const newLi =`<li class="list-group-item fs-5">
   <i class="fas fa-bicycle"></i>
   ${x.sna.replace("YouBike2.0_","")} (${x.sbi})<br>
   <small class="text-muted">${x.ar})</small>
   </li>` 
-  return bike
+
+  return newLi
 }
